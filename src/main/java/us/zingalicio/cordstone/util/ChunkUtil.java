de76e7b3da0455c7b16021885bb7a9a3e6dbf5a6
zingten11 <admin@zingalicio.us>
2014-08-21 21:24:57 -0700

    Added ChunkUtil

---
+++ src/main/java/us/zingalicio/cordstone/util/ChunkUtil.java
@@ -0,0 +1,26 @@
+package us.zingalicio.cordstone.util;
+
+import java.util.ArrayList;
+
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+
+public class ChunkUtil 
+{
+	public static ArrayList<Chunk> getChunks(Location loc, int radius)
+	{
+		ArrayList<Chunk> chunks = new ArrayList<>(((radius * 2) + 1) * ((radius * 2) + 1));
+		int x = loc.getBlockX();
+		int z = loc.getBlockZ();
+		int dX = radius * -1;
+		while(dX <= radius)
+		{
+			int dZ = radius * -1;
+			while(dZ <= radius)
+			{
+				chunks.add(new Location(loc.getWorld(), x + (dX * 16), 1, z + (dZ * 16)).getChunk());
+			}
+		}
+		return chunks;
+	}
+}
