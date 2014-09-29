16feee353a437ea49f78ab68bc3330d81b45d018
zingten11 <zingten11@gmail.com>
2014-09-28 22:43:05 -0700

    Removed craftbukkit dependency since that's no longer anywhere and was unneeded as of now

--- src/main/java/us/zingalicio/cordstone/exceptions/NoConsoleException.java
+++ src/main/java/us/zingalicio/cordstone/exceptions/NoConsoleException.java
@@ -3,4 +3,9 @@ package us.zingalicio.cordstone.exceptions;
 public class NoConsoleException extends Exception
 {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4291044287724409360L;
+
 }
